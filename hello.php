d4d6dc57ed1309a2fb610821f3d44d81dc867c99
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:56:23 +0700

    dfgdfg

--- hello.php
+++ hello.php
@@ -1,17 +1,23 @@
+
+
+
 <?php
-$servername = "us-cdbr-east-05.cleardb.net";
-$username = "b4e68142b2db41";
-$password = "60a01a12";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e57", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
-  $array = $pdo->query("SELECT * FROM table sinhvien")->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($array);
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
-
+    // mở kết nối tới mysql db
+    $connection = mysqli_connect ("us-cdbr-east-05.cleardb.net", "b4e68142b2db41", "60a01a12", "heroku_2b76120d7232e57") ordie ("Error". mysqli_error ($connection));
+?>
+<?php
+    //fetch table rows from mysql db
+    $sql = "select * from sinhvien";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+?>
+<?php
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+?>
+<?php 
+    echo json_encode($emparray);
 ?>
