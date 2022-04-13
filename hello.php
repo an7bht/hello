7234055d61478422156082f75c7a145a1f4e4bdf
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:43:19 +0700

    dfdsf

--- hello.php
+++ hello.php
@@ -4,21 +4,23 @@ $username = "b4e68142b2db41";
 $password = "60a01a12";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e5", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e57", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfullyyyyyy";
-  $sql = "SELECT * FROM sinhvien";
-// thực hiện cầu lên truy vấn và kết nối
-    $result =new PDO(mysqli_query($conn, $sql));
-// tạo một mảng
-    $arr = array();
+  echo "Connected successfully";
 
-    while ($rows = mysqli_fetch_assoc($result)) {
-	array_push($arr, $rows);
-        }
-// xuất json
-    echo json_encode($arr);
+  $return_arr = array();
+
+    $fetch = mysql_query("SELECT * FROM sinhvien"); 
+
+    while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
+        $row_array['MSSV'] = $row['MSSV'];
+       
+
+        array_push($return_arr,$row_array);
+    }
+
+    echo json_encode($return_arr);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
