ef3bd17ce0a7f7492c4383735b4c409fb46dbc11
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:24:25 +0700

    gfsdg

--- hello.php
+++ hello.php
@@ -1,1 +1,17 @@
-fadadfasd
+<?php 
+// kết nối tới mysql
+$conn = mysqli_connect("localhost", "root", "123456", "sqlquanlysinhvien");
+// câu lệnh truy vấn sql
+$sql = "SELECT * FROM sinhvien";
+// thực hiện cầu lên truy vấn và kết nối
+$result = mysqli_query($conn, $sql);
+// tạo một mảng
+$arr = array();
+
+while ($rows = mysqli_fetch_assoc($result)) {
+	array_push($arr, $rows);
+}
+// xuất json
+echo json_encode($arr);
+
+?>
