0284b2247a52a6c121977abfa7773d30be9c5078
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:39:38 +0700

    gfdfgdsggd

--- hello.php
+++ hello.php
@@ -7,18 +7,18 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e57", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  echo "Connected successfullyyyyyy";
   $sql = "SELECT * FROM sinhvien";
 // thực hiện cầu lên truy vấn và kết nối
-$result = mysqli_query($conn, $sql);
+    $result =new PDO(mysqli_query($conn, $sql));
 // tạo một mảng
-$arr = array();
+    $arr = array();
 
-while ($rows = mysqli_fetch_assoc($result)) {
+    while ($rows = mysqli_fetch_assoc($result)) {
 	array_push($arr, $rows);
-}
+        }
 // xuất json
-echo json_encode($arr);
+    echo json_encode($arr);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
