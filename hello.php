46d51018bfb019f327df5197bc93b7ede7fd947a
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 15:01:06 +0700

    gdfgdf

--- hello.php
+++ hello.php
@@ -12,7 +12,7 @@
     {
         $emparray[] = $row;
     }
-    echo json_encode($emparray);
+    echo json_encode($emparray)."thanh cong";
 
     //close the db connection
     mysqli_close($connection);
