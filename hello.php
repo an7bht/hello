7bcdd8cca923898fef823e6f30eafabe2abd0920
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:32:55 +0700

    hjfghfgh

--- hello.php
+++ hello.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "123456";
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "b4e68142b2db41";
+$password = "60a01a12";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e57", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
