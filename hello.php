ac06efbe4af2067167e93c9afee6dc89bd59efbc
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:59:38 +0700

    dfsdf

--- hello.php
+++ hello.php
@@ -1,23 +1,19 @@
-
-
-
 <?php
-    // mở kết nối tới mysql db
-    $connection = mysqli_connect ("us-cdbr-east-05.cleardb.net", "b4e68142b2db41", "60a01a12", "heroku_2b76120d7232e57") ordie ("Error". mysqli_error ($connection));
-?>
-<?php
+    //open connection to mysql db
+    $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","b4e68142b2db41","60a01a12","heroku_2b76120d7232e57") or die("Error " . mysqli_error($connection));
+
     //fetch table rows from mysql db
     $sql = "select * from sinhvien";
-    $result = mysqli_query($connection, $sql) ordie("Error in Selecting " . mysqli_error($connection));
-?>
-<?php
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
     //create an array
     $emparray = array();
     while($row =mysqli_fetch_assoc($result))
     {
         $emparray[] = $row;
     }
-?>
-<?php 
     echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($connection);
 ?>
