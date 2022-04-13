0d24f6e6edcb26c80bc6df35dc9d6dc84c3c8829
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:47:35 +0700

    fsdf

--- hello.php
+++ hello.php
@@ -7,20 +7,9 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=heroku_2b76120d7232e57", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "Connected successfully";
-
-  $return_arr = array();
-
-    $fetch = mysql_query("SELECT * FROM sinhvien"); 
-
-    while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
-        $row_array['MSSV'] = $row['MSSV'];
-       
-
-        array_push($return_arr,$row_array);
-    }
-
-    echo json_encode($return_arr);
+  echo "Connected successfully";
+  $array = $pdo->query("SELECT * FROM table sinhvien")->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($array);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
