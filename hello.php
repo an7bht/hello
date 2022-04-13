dab631c7ccfbfab77c4d4b327d28db26f359b182
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:34:37 +0700

    gfhfghfg

--- hello.php
+++ hello.php
@@ -8,7 +8,19 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
+  $sql = "SELECT * FROM db";
+// thực hiện cầu lên truy vấn và kết nối
+$result = mysqli_query($conn, $sql);
+// tạo một mảng
+$arr = array();
+
+while ($rows = mysqli_fetch_assoc($result)) {
+	array_push($arr, $rows);
+}
+// xuất json
+echo json_encode($arr);
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
