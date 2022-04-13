fbe1b93778b1868647bfa384ce88afdbf5a13233
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 16:54:06 +0700

    dsd

--- hello.php
+++ hello.php
@@ -1,9 +1,10 @@
 <?php
     //open connection to mysql db
+    // $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","b4e68142b2db41","60a01a12","heroku_2b76120d7232e57") or die("Error " . mysqli_error($connection));
     $connection = mysqli_connect("us-cdbr-east-05.cleardb.net","b4e68142b2db41","60a01a12","heroku_2b76120d7232e57") or die("Error " . mysqli_error($connection));
 
     //fetch table rows from mysql db
-    $sql = "select * from taikhoansv";
+    $sql = "select DiaChi from sinhvien";
     $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
 
     //create an array
