69b9635dabc0985aecd954c3fd665e3e1ae08486
AnDang <81478990+an7bht@users.noreply.github.com>
2022-04-13 14:31:33 +0700

    gdfgd

--- hello.php
+++ hello.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "123456";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=sqlquanlyhocphan", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
